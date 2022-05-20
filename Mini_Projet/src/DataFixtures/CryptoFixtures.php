038aba901c48f70611feb09ac7cb9501efd9a35d
enoraLD <enora.langard@gmail.com>
2022-05-20 16:11:20 +0200

    mise à jour

--- Mini_Projet/src/DataFixtures/CryptoFixtures.php
+++ Mini_Projet/src/DataFixtures/CryptoFixtures.php
@@ -15,10 +15,11 @@ class CryptoFixtures extends Fixture
         $ch = curl_init();
         try {
             curl_setopt($ch, CURLOPT_URL, "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false%22");
+           // curl_setopt($ch, CURLOPT_URL,"https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&category=cryptocurrency&order=market_cap_desc&per_page=50&page=$i&sparkline=false")
             curl_setopt($ch, CURLOPT_HEADER, false);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -47,8 +48,8 @@ class CryptoFixtures extends Fixture
             curl_setopt($ch2, CURLOPT_URL, "https://api.coingecko.com/api/v3/coins/categories/list");
             curl_setopt($ch2, CURLOPT_HEADER, false);
             curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch2, CURLOPT_CONNECTTIMEOUT, 30);
-            curl_setopt($ch2, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch2, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($ch2, CURLOPT_TIMEOUT, 5);
             curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($ch2, CURLOPT_MAXREDIRS, 1);
             curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
@@ -75,26 +76,27 @@ class CryptoFixtures extends Fixture
         $json = json_decode($response, 1);
         $json2 = json_decode($response2, 1);
 
+        foreach ($json2 as $j2){
+            
+        }
+
         foreach ($json as $j){
 
             $crypto = new Crypto();
             $crypto->setNom($j["name"]);
             $crypto->setSymbole($j["symbol"]);
-           $crypto->SetImage($j["image"]);
+            $crypto->SetImage($j["image"]);
             $crypto->setPrix($j["current_price"]);
             $crypto->setDescription("");
+            $crypto->setCategorie("");
             $crypto->setMarketcup($j["market_cap"]);
             $crypto->setNbFollowers(0);
-
+    
             $manager->persist($crypto);
         }
-
-        foreach ($json2 as $j2){
-            $crypto->setCategorie($j2["name"]);
-            $manager->persist($crypto);
-        }
-
+        
         $manager->flush();
+
 
     }
 
